fa36124280804e52df9540184eefd8254ded7e1c
Ahmad Hassan <146438862+ahmadhassan44@users.noreply.github.com>
2025-02-24 18:54:53 +0500

    chore/use dto instead of library's Body

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,16 @@
 import { Body, Controller, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller("auth")
 export class AuthController{
     constructor(private authService: AuthService) {}
     @Post("signup")
-    signup(@Body() body:any){
+    signup(@Body() body:AuthDto){
         this.authService.signup()
     }
     @Post("signin")
-    signin(@Body() body:any){
+    signin(@Body() body:AuthDto){
         this.authService.signin()
     }
 }
