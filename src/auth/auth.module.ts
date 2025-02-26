70f1b0d48734b71b4f03c44e2b42a531a175c488
Ahmad Hassan <146438862+ahmadhassan44@users.noreply.github.com>
2025-02-26 21:13:42 +0500

    feat/inject both starategies as providers to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,13 @@
-import { Module } from "@nestjs/common";
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
-import { PrismaModule } from "src/prisma/prisma.module";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AtStrategy, RtStrategy } from './strategy';
 
 @Module({
-    controllers :[AuthController],
-    providers: [AuthService]
+  controllers: [AuthController],
+  providers: [AuthService, AtStrategy, RtStrategy],
 })
-export class AuthModule{
-    constructor(private prismaService: PrismaService){
-
-    }
-
+export class AuthModule {
+  constructor(private prismaService: PrismaService) {}
 }
