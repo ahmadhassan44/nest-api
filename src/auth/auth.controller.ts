104cf4480282e9f48a8fd0880ba59276e23d0341
Ahmad Hassan <146438862+ahmadhassan44@users.noreply.github.com>
2025-02-27 18:14:48 +0500

    feat/add status code decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto, SignUpDto } from './dto';
 import { Tokens } from './types';
@@ -8,14 +15,17 @@ import { SignOutDto } from './dto/signout.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
   signup(@Body() body: SignUpDto): Promise<Tokens> {
     return this.authService.signup(body);
   }
   @Post('signin')
+  @HttpCode(HttpStatus.OK)
   signin(@Body() body: SignInDto): Promise<Tokens> {
     return this.authService.signin(body);
   }
   @Post('logout')
+  @HttpCode(HttpStatus.OK)
   logout(@Body() body: SignOutDto): Promise<void> {
     return this.authService.logout(body);
   }
